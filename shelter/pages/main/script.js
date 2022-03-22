0bc4808761c7b5b82fb3e35164fe43cb41f7cb3b
thelastandrew <thelastandrew@gmail.com>
2022-03-22 06:31:52 +0300

    feat: add underline functionality for nav bar

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,12 @@
+const navItem = document.querySelectorAll('.nav-item');
+let activeNavItem = document.querySelector('.active-nav');
+
+navItem.forEach((element) => {
+  element.addEventListener('click', function (e) {
+    if (!element.classList.contains('active-nav')) {
+      activeNavItem.classList.remove('active-nav');
+      element.classList.add('active-nav');
+      activeNavItem = element;
+    }
+  });
+});
