f6a5075e98f5b14be751ce65fb9a1326785d9a09
thelastandrew <thelastandrew@gmail.com>
2022-04-23 10:37:22 +0300

    feat: add 'infitite' option to slider

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -40,9 +40,6 @@ const cards = document.querySelector('.slider-cards');
 const leftBtn = document.querySelector('.left-btn');
 const rightBtn = document.querySelector('.right-btn');
 let cardArray = document.querySelectorAll('.card');
-let shownCards = Math.floor(
-  document.querySelector('.slider-frame').clientWidth / 270
-);
 
 const moveLeft = () => {
   cards.classList.add('transition-left');
@@ -55,15 +52,33 @@ const moveRight = () => {
   rightBtn.removeEventListener('click', moveRight);
 };
 
+function shiftCards(direction, step) {
+  for (let i = 0; i < step; i++) {
+    if (direction === 'left') {
+      cards.insertBefore(cardArray[cardArray.length - 1 - i], cardArray[0]);
+    } else {
+      cards.appendChild(cardArray[i]);
+    }
+  }
+  cardArray = document.querySelectorAll('.card');
+}
+
 leftBtn.addEventListener('click', moveLeft);
 rightBtn.addEventListener('click', moveRight);
 
 cards.addEventListener('animationend', (animationEvent) => {
+  let dir;
+  let shownCards = Math.floor(
+    document.querySelector('.slider-frame').clientWidth / 270
+  );
   if (animationEvent.animationName === 'move-left') {
+    dir = 'left';
     cards.classList.remove('transition-left');
   } else {
+    dir = 'right';
     cards.classList.remove('transition-right');
   }
+  shiftCards(dir, shownCards);
   leftBtn.addEventListener('click', moveLeft);
   rightBtn.addEventListener('click', moveRight);
 });
